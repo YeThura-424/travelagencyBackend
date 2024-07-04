a21218d8790c6295b6672bd77e5fef1178b6d5bc
YeThura-424 <rathanpoudle.26@gmail.com>
2024-07-04 21:59:06 +0630

    destination model

---
+++ app/Models/Destination.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Destination extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $table = 'destinations';
+    protected $connection = 'mysql';
+
+
+    protected $fillable = [
+        'name','location','description','images','status','created_by','updated_by'
+    ];
+
+    protected $casts = [
+        'location' => 'json',
+    ];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        self::creating(function($model){
+            $model->created_by = auth()->id();
+        });
+
+        self::updating(function($model) {
+            $model->updated_by = auth()->id();
+        });
+    }
+
+}
