baad3bc8d3da694a90ce8d2451e221f3917d89c8
YeThura-424 <rathanpoudle.26@gmail.com>
2024-07-11 21:29:32 +0630

    update location service methods

--- app/Services/LocationService.php
+++ app/Services/LocationService.php
@@ -2,73 +2,29 @@
 
 namespace App\Services;
 
-use function PHPSTORM_META\map;
+use App\Models\Town;
+use App\Models\Region;
+use App\Models\Township;
+use App\Models\TownshipFullInfo;
 
 class LocationService
 {
-
-  protected $rawLocation;
-  public function __construct()
-  {
-    $this->rawLocation = collect(config('region.data'));
-  }
   public function getRegion()
   {
-    $region = [];
-    foreach ($this->rawLocation as $location) {
-
-      $region[] = [
-        'name_en' => $location['eng'],
-        'name_mm' => $location['mm'],
-        'lat' => $location['lat'],
-        'lng' => $location['lng']
-      ];
-    }
-    return $region;
+    return Region::all();
   }
 
   public function getTown()
   {
-    $town = [];
-    $region_id = 1;
-    foreach ($this->rawLocation as $location) {
-      $id = 1;
-      foreach ($location['districts'] as $data) {
-        $town[] = [
-          'id' => $id,
-          'region_id' => $region_id,
-          'name_en' => $data['eng'],
-          'name_mm' => $data['mm']
-        ];
-        $id++;
-      }
-      $region_id++;
-    }
-    return $town;
+    return Town::all();
   }
 
   public function getTownship()
   {
-    $township = [];
-    $region_id = 1;
-    foreach ($this->rawLocation as $location) {
-      $town_id = 1;
-      foreach ($location['districts'] as $towns) {
-        $id = 1;
-        foreach ($towns['townships'] as $data) {
-          $township[] = [
-            'id' => $id,
-            'region_id' => $region_id,
-            'town_id' => $town_id,
-            'name_en' => $data['eng'],
-            'name_mm' => $data['mm']
-          ];
-          $id++;
-        }
-        $town_id++;
-      }
-      $region_id++;
-    }
-    return $township;
+    return Township::all();
+  }
+  public function getTownshipFullInfo()
+  {
+    return TownshipFullInfo::all();
   }
 }
