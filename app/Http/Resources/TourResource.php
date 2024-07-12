67ff88dcd95d2965a430aeaaaf272604e3083e2b
YeThura-424 <rathanpoudle.26@gmail.com>
2024-07-12 21:57:19 +0630

    tour resource

--- app/Http/Resources/TourResource.php
+++ app/Http/Resources/TourResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,17 @@ class TourResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'tour_name' => $this->name,
+            'tour_description' => $this->descriptin,
+            'tour_price' => $this->price,
+            'duration' => $this->duration,
+            'start_date' => $this->start_date->format('d-m-Y'),
+            'end_date' => $this->end_date->format('d-m-Y'),
+            'max_people' => $this->max_people,
+            'status' => $this->status,
+            'created_by' => new UserResource(User::find($this->created_by)),
+            'updated_by' => new UserResource(User::find($this->updated_by)),
+        ];
     }
 }
