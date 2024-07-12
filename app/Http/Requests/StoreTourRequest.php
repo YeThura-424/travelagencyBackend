aa2fd0083dd48c3163c9a2d6796b49875a53671b
YeThura-424 <rathanpoudle.26@gmail.com>
2024-07-12 22:15:10 +0630

    update: storetourrequest

--- app/Http/Requests/StoreTourRequest.php
+++ app/Http/Requests/StoreTourRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreTourRequest.php extends FormRequest
+class StoreTourRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,7 +22,13 @@ class StoreTourRequest.php extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'unique:tours.name'],
+            'description' => 'required',
+            'price' => 'required|numeric',
+            'duration' => 'required|numeric',
+            'start_date' => 'required',
+            'end_date' => 'required',
+            'max_people' => 'required|numeric'
         ];
     }
 }
